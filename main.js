d3a5b0611f78371e9fd23244ecacb9ddf0877235
isaHgodoi <106815233+isaHgodoi@users.noreply.github.com>
2024-04-22 10:32:28 -0300

    zzz

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ function verificarTime() {
 
   while (continuarPerguntando) {
     
-    var respostaTime = prompt("Qual o melhor time?");
+    var respostaTime = prompt("Onde podemos levar noso animalzinho quando eles estão doentes?");
 
     
-    if (respostaTime.toLowerCase() === "athlético pr") {
-      alert("Isso mesmo! O Athlético PR é o melhor time!");
+    if (respostaTime.toLowerCase() === "Podemos levá-los no veteririo") {
+      alert("Isso mesmo! No veterinario!");
       continuarPerguntando = false; // Sai do loop após a resposta correta
     } else {
-      alert("Não é isso! Tente novamente.");
+      alert("Inincorreto! Tente novamente.");
     }
   }
 }
